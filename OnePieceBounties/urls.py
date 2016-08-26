05067a11f7dfc819c206de32a1a56f7bcf406d4f
kgmfigueroa <kathleengay.figueroa@gmail.com>
2016-08-26 13:19:06 +0800

    Refactor url dispatcher. Move to bounties app.

--- OnePieceBounties/urls.py
+++ OnePieceBounties/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from bounties import views
+from bounties import urls as bounties_urls
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'OnePieceBounties.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^onepiecebounties/', views.index, name='index'),
+    url(r'^onepiecebounties/', include(bounties_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
