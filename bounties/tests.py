e814e6e75cc4ac603f2fd4378ff73b1e22b334aa
kgmfigueroa <kathleengay.figueroa@gmail.com>
2016-08-26 15:48:23 +0800

    Add prefix of component being tested to test methods and move to classes

--- bounties/tests.py
+++ bounties/tests.py
@@ -5,15 +5,15 @@ from .models import Pirate
 
 class HomePageTest(TestCase):
 
-    def test_can_resolve_to_home_page_view(self):
+    def test_url_can_resolve_to_home_page_view(self):
         found = resolve('/onepiecebounties/')
         self.assertEqual(found.func, index)
 
-    def test_returns_home_template(self):
+    def test_template_returns_home_template(self):
         response = self.client.get('/onepiecebounties/')
         self.assertTemplateUsed(response, 'index.html')
 
-    def test_returns_OK(self):
+    def test_view_returns_OK(self):
         response = self.client.get('/onepiecebounties/')
         self.assertEqual(response.status_code, 200)
 
@@ -22,21 +22,39 @@ class HomePageTest(TestCase):
         luffy.save()
         alvida = Pirate(name='Iron Mace Alvida')
         alvida.save()
+        shanks = Pirate(name='Red-Hair Shanks')
+        shanks.save()
         
         response = self.client.get('/onepiecebounties/')
         context = response.context['pirates']
         
         self.assertEqual(context.get(id=1), luffy)
         self.assertEqual(context.get(id=2), alvida)
+        self.assertEqual(context.get(id=3), shanks)
 
-    def test_save_and_retrieve_pirates(self):
+    def test_template_shows_all_pirates(self):
+        luffy = Pirate(name='Monkey D. Luffy')
+        luffy.save()
+        alvida = Pirate(name='Iron Mace Alvida')
+        alvida.save()
+        shanks = Pirate(name='Red-Hair Shanks')
+        shanks.save()
+        
+        response = self.client.get('/onepiecebounties/')
+        self.assertContains(response, luffy.name)
+        self.assertContains(response, alvida.name)
+        self.assertContains(response, shanks.name)
+
+class PirateModelTest(TestCase):
+
+    def test_model_save_and_retrieve_pirates(self):
         luffy = Pirate(name='Monkey D. Luffy')
         luffy.save()
         
         first = Pirate.objects.first()
         self.assertEqual(first.name, luffy.name)
         
-    def test_save_and_retrieve_multiple_pirates(self):
+    def test_model_can_save_and_retrieve_multiple_pirates(self):
         luffy = Pirate(name='Monkey D. Luffy')
         luffy.save()
         
@@ -45,3 +63,4 @@ class HomePageTest(TestCase):
 
         pirates = Pirate.objects.all()
         self.assertEqual(pirates.count(), 2)
+
